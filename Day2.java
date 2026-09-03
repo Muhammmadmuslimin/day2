3dd64ba5562453e9242651aa67e149b54b1cb4cc
Muhammmadmuslimin <stokzann011@gmail.com>
2026-09-03 16:40:30 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+      int umur = 20;
+        char kelamin = 'l';
+        double tinggi = 170.0;
+        float berat = 60.0f;
+        
+        String nama = "Muhammad Muslimin";
+        
+        System.out.println("nama saya" + nama);
+        System.out.println("umur saya" + umur);
+        System.out.println("tinggi aku" + tinggi);
+        System.out.println("berat saya" + berat);
+        
+    }
+    
+}
